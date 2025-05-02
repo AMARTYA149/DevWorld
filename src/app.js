bf36e20d43657452f583ffaae7bbfa557ad58e0d
AMARTYA149 <amartya970914@gmail.com>
2025-05-02 17:48:03 +0530

    Handling multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get("/ab+c",(req, res)=>{
-    res.send("Hello from server!!!");
-});
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling route handler 1");
+    next();
+  },
+  [
+    (req, res, next) => {
+      console.log("Handling route handler 2");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Handling route handler 3");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    console.log("Handling route handler 4");
+    res.send("4th Response");
+  }
+);
 
-app.listen(7777, ()=>{
-    console.log("Server running");
+app.listen(7777, () => {
+  console.log("Server running");
 });
