abd132909cb5dc5a71796ab94c3f7726951a5946
AMARTYA149 <amartya970914@gmail.com>
2025-05-13 22:22:41 +0530

    using express.Router()

--- src/app.js
+++ src/app.js
@@ -1,13 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/database.js");
 const app = express();
-const User = require("./models/user.js");
-const { validateSignupData } = require("./utils/validation.js");
-const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");
-const JWT_SECRET_WORD = "Dev@World_149";
-const { userAuth } = require("./middlewares/auth.js");
 
 // Route Handlers with =>
 // 2 parameters -
@@ -20,80 +14,13 @@ const { userAuth } = require("./middlewares/auth.js");
 app.use(express.json());
 app.use(cookieParser());
 
-app.post("/signup", async (req, res) => {
-  try {
-    // validation of data
-    validateSignupData(req);
-    const { firstName, lastName, emailId, password } = req.body;
+const authRouter = require("./routes/authRouter.js");
+const profileRouter = require("./routes/profileRouter.js");
+const requestRouter = require("./routes/requestRouter.js");
 
-    // encrypt the password
-    const passwordHash = await bcrypt.hash(password, 10);
-
-    // Creating a new instance of the User model
-    const user = new User({
-      firstName,
-      lastName,
-      emailId,
-      password: passwordHash,
-    });
-    if (user.skills) {
-      user.skills = [...new Set(user.skills.map((item) => item.trim()))];
-      if (user.skills.length > 5) {
-        throw new Error("Skills can not be more than 5");
-      }
-    }
-    await user.save();
-    res.send("User added successfully!");
-  } catch (error) {
-    res.status(500).send(`${error}`);
-  }
-});
-
-app.post("/login", async (req, res) => {
-  const { emailId, password } = req.body;
-
-  const user = await User.findOne({ emailId });
-
-  try {
-    if (!user) {
-      throw new Error("Invalid Credentials!");
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-
-    if (isPasswordValid) {
-      // Create a JWT token
-
-      const token = await user.getJWT(); // generate JWT token of the loggedin user through the _id of the user loggedin
-
-      // Add token to the cookie and send the response back to the client
-      res.cookie("token", token, {
-        expires: new Date(Date.now() + 900000),
-        httpOnly: true,
-      });
-      res.send("Login Successful!");
-    } else {
-      throw new Error("Invalid Credentials!");
-    }
-  } catch (error) {
-    res.status(500).send(`${error}`);
-  }
-});
-
-app.get("/profile", userAuth, async (req, res) => {
-  try {
-    const user = req.user;
-    res.send(user);
-  } catch (error) {
-    res.status(500).send("ERROR: " + error.message);
-  }
-});
-
-app.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  const user = req.user;
-
-  res.send(user.firstName + " send the connection request!");
-});
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
 
 connectDB()
   .then(() => {
---
+++ src/routes/authRouter.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const authRouter = express.Router();
+const { validateSignupData } = require("../utils/validation.js");
+const bcrypt = require("bcrypt");
+const User = require("../models/user.js");
+
+authRouter.post("/signup", async (req, res) => {
+  try {
+    // validation of data
+    validateSignupData(req);
+    const { firstName, lastName, emailId, password } = req.body;
+
+    // encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    // Creating a new instance of the User model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+    if (user.skills) {
+      user.skills = [...new Set(user.skills.map((item) => item.trim()))];
+      if (user.skills.length > 5) {
+        throw new Error("Skills can not be more than 5");
+      }
+    }
+    await user.save();
+    res.send("User added successfully!");
+  } catch (error) {
+    res.status(500).send(`${error}`);
+  }
+});
+
+authRouter.post("/login", async (req, res) => {
+  const { emailId, password } = req.body;
+
+  const user = await User.findOne({ emailId });
+
+  try {
+    if (!user) {
+      throw new Error("Invalid Credentials!");
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (isPasswordValid) {
+      // Create a JWT token
+
+      const token = await user.getJWT(); // generate JWT token of the loggedin user through the _id of the user loggedin
+
+      // Add token to the cookie and send the response back to the client
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 900000),
+        httpOnly: true,
+      });
+      res.send("Login Successful!");
+    } else {
+      throw new Error("Invalid Credentials!");
+    }
+  } catch (error) {
+    res.status(500).send(`${error}`);
+  }
+});
+
+module.exports = authRouter;
