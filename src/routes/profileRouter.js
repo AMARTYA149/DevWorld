55fa2c07d04fff8610a039d98a97ab57240eb51e
AMARTYA149 <amartya970914@gmail.com>
2025-05-13 23:24:11 +0530

    logout + validateEditProfile + edit profile

--- src/routes/profileRouter.js
+++ src/routes/profileRouter.js
@@ -1,14 +1,35 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth.js");
+const { validateEditProfileData } = require("../utils/validation.js");
 
 const profileRouter = express.Router();
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
     res.send(user);
   } catch (error) {
     res.status(500).send("ERROR: " + error.message);
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid edit request!");
+    }
+    const loggedInUser = req.user;
+
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+
+    await loggedInUser.save();
+
+    res.json({
+      message: `${loggedInUser.firstName}, your profile has been updated successfully!`,
+      data: loggedInUser,
+    });
+  } catch (error) {
+    res.status(400).send("ERROR: " + error.message);
   }
 });
 
