f6ba7666764e5bfa2a13445a4092bcaf14420d24
AMARTYA149 <amartya970914@gmail.com>
2025-05-21 17:05:52 +0530

    EP-13: Connection request-accepted or rejected

--- src/routes/requestRouter.js
+++ src/routes/requestRouter.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth.js");
-const ConnectionRequestModel = require("../models/connectionRequest.js");
+const ConnectionRequest = require("../models/connectionRequest.js");
 const User = require("../models/user.js");
 const requestRouter = express.Router();
+const mongoose = require("mongoose");
+
 
 requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
 
   try {
     const fromUserId = req.user._id;
-    const toUserId = req.params.toUserId;
-    const status = req.params.status;
+    const {
+      toUserId, status
+    } = req.params;
 
     const allowedStatus = ["ignored", "interested"];
-
 
     if (!allowedStatus.includes(status)) {
       return res.status(400).json({
@@ -27,12 +29,12 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
       })
     }
 
-    const connectionRequest = new ConnectionRequestModel({
+    const connectionRequest = new ConnectionRequest({
       fromUserId, toUserId, status
     });
 
     // check if there is an existing connectionRequest
-    const existingConnectionRequest = await ConnectionRequestModel.findOne({
+    const existingConnectionRequest = await ConnectionRequest.findOne({
       $or: [
         { fromUserId, toUserId },
         { fromUserId: toUserId, toUserId: fromUserId }
@@ -58,5 +60,48 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
     res.status(400).send("ERROR: " + error.message);
   }
 });
+
+requestRouter.post("/request/review/:status/:requestId", userAuth, async (req, res) => {
+  try {
+    // Babita=>Jay
+    // loggedInUser == toUserId
+    // status ===  interested
+    // requestId should be valid
+    const loggedInUser = req.user;
+    const { status, requestId } = req.params;
+    const allowedStatus = ["accepted", "rejected"];
+
+    if (!allowedStatus.includes(status)) {
+      return res.status(400).json({
+        message: "Status not allowed!"
+      });
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(requestId)) {
+      return res.status(400).json({ message: "Invalid request ID" });
+    }
+
+    const connectionRequest = await ConnectionRequest.findOne({
+      _id: requestId,
+      status: "interested",
+      toUserId: loggedInUser._id
+    });
+
+    if (!connectionRequest) {
+      return res.status(404).json({ message: "Connection request not found!" });
+    }
+
+    connectionRequest.status = status;
+    const data = await connectionRequest.save();
+
+    res.json({
+      message: `Connection request ${status}`,
+      data
+    });
+
+  } catch (error) {
+    res.status(400).send("ERROR: " + error.message);
+  }
+})
 
 module.exports = requestRouter;
