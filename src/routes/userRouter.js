e39bd57c71a07c35f44817971269f2b40140ca5d
AMARTYA149 <amartya970914@gmail.com>
2025-05-24 19:43:31 +0530

    S-2 Ep-14: Feed API wip

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { userAuth } = require('../middlewares/auth');
 const userRouter = express.Router();
 const ConnectionRequest = require("../models/connectionRequest.js");
+const User = require("../models/user.js");
 const USER_SAFE_DATA = "firstName lastName photoUrl skills about age gender";
 
 
@@ -53,6 +54,53 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     } catch (error) {
         res.status(400).send("ERROR: " + error.message);
     }
-})
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+
+    // LoggedInUser should see all the users except
+    // 1. his own profile
+    // 2. profiles he accepted
+    // 3. profiles he rejected
+    // 4. profiles he send request
+    try {
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [{
+                fromUserId: loggedInUser._id
+            }, {
+                toUserId: loggedInUser._id
+            }]
+        }).select("fromUserId toUserId");
+        // .populate("fromUserId", "firstName").populate("toUserId", "firstName");
+
+        const hideUsersFromFeed = new Set();
+
+        connectionRequests.forEach((connections) => {
+            hideUsersFromFeed.add(connections.fromUserId.toString());
+            hideUsersFromFeed.add(connections.toUserId.toString());
+        });
+
+        const users = await User.find({
+            $and: [{
+                _id: {
+                    $nin: Array.from(hideUsersFromFeed)
+                }
+            }, {
+                _id: {
+                    $ne: loggedInUser._id
+                }
+            }]
+        }).select(USER_SAFE_DATA);
+
+        res.json({ data: users });
+
+    } catch (error) {
+        res.status(400).send("ERROR: " + error.message);
+    }
+});
+
+
 
 module.exports = userRouter;
