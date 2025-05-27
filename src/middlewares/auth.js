ead14c10e2e452bd909fb4332b13afd7701344b6
AMARTYA149 <amartya970914@gmail.com>
2025-05-27 21:51:45 +0530

    S-2 Ep17 login status correction

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -9,7 +9,7 @@ const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
     if (!token) {
-      throw new Error("Invalid token!!!");
+      return res.status(401).send({ message: "Please login!" });
     }
 
     const decodedObj = await jwt.verify(token, JWT_SECRET_WORD);
