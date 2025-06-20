1f020201977850f9ab263773cbf932dcf277ced4
AMARTYA149 <amartya970914@gmail.com>
2025-06-20 09:28:00 +0530

    S-3 Ep-6 npm i Sending email using cron-jobs

--- src/utils/cronjob.js
+++ src/utils/cronjob.js
@@ -1,15 +1,37 @@
 const cron = require("node-cron");
 const ConnectionRequestModel = require("../models/connectionRequest");
+const { subDays, startOfDay, endOfDay } = require("date-fns");
+const sendEmail = require("./sendEmail");
 
-cron.schedule("* * * * * *", () => {
+cron.schedule("0 9 * * *", async () => { // this job will run 9am every morning
     // Send email to all people who have received requests the previous day
+    console.log("Cron job");
     try {
-        const yesterday = new Date();
-        const pendingRequests = await ConnectionRequestModel.find({
-            status: interested,
-            createdAt: 
-        })
-    } catch (err) {
+        const yesterday = subDays(new Date(), 1);
+        const yesterdayStart = startOfDay(yesterday);
+        const yesterdayEnd = endOfDay(yesterday);
 
+        const pendingRequestsOfYesterday = await ConnectionRequestModel.find({
+            status: "interested",
+            createdAt: {
+                $gte: yesterdayStart,
+                $lt: yesterdayEnd
+            }
+        }).populate("fromUserId toUserId");
+
+        const listOfEmails = [...new Set(pendingRequestsOfYesterday.map(req => req.toUserId.emailId))];
+
+        for (const email of listOfEmails) {
+            // Send Emails
+            try {
+                const res = await sendEmail.run("New friend requests received for " + email, "New friends are waiting to connect to you @ DevWorld!");
+
+
+            } catch (error) {
+                console.log("Error fetching email id");
+            }
+        }
+    } catch (err) {
+        console.log("Error occured");
     }
 })
